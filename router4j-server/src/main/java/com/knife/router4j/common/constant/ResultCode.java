35b13b7215136fe3e441b89e8cca93b6ff65ec61
water <2716552863@qq.com>
2022-04-17 23:38:21 +0800

    添加公共代码等

---
+++ router4j-server/src/main/java/com/knife/router4j/common/constant/ResultCode.java
@@ -0,0 +1,23 @@
+package com.knife.router4j.common.constant;
+ 
+public enum ResultCode {
+    SUCCESS(1000, "访问成功"),
+    SYSTEM_FAILURE(1001, "系统异常"),
+    ;
+ 
+    private final int code;
+    private final String description;
+ 
+    ResultCode(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+ 
+    public int getCode() {
+        return code;
+    }
+ 
+    public String getDescription() {
+        return description;
+    }
+}
