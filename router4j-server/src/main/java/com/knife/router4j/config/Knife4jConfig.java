f799ab87440f260b3772e8830278d8a69f73a1e7
water <2716552863@qq.com>
2022-04-17 22:04:01 +0800

    上传初版代码结构

---
+++ router4j-server/src/main/java/com/knife/router4j/config/Knife4jConfig.java
@@ -0,0 +1,40 @@
+package com.knife.router4j.config;
+ 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+ 
+@Configuration
+@EnableOpenApi
+// @EnableKnife4j
+// @EnableSwagger2
+public class Knife4jConfig {
+    @Bean
+    public Docket docket() {
+        Docket docket = new Docket(DocumentationType.OAS_30)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("我的标题")
+                        .description("我的描述")
+                        // .termsOfServiceUrl("http://www.xx.com/")
+                        .contact(new Contact("knife", "https://knife.blog.csdn.net/", "xx@qq.com"))
+                        .version("1.0")
+                        .build())
+                // 分组名称
+                .groupName("all")
+                .select()
+                // 这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("com.knife.router4j"))
+                .paths(PathSelectors.any())
+                .build();
+ 
+        return docket;
+    }
+}
---
+++ router4j-server/src/test/java/com/knife/router4j/Router4jApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.knife.router4j;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class Router4jApplicationTests {
+//
+//     @Test
+//     void contextLoads() {
+//     }
+//
+// }
