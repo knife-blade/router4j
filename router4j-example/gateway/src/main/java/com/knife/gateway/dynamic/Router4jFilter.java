00b5535708c7981fcd97c74ed1f823dd6d2cec6c
water <2716552863@qq.com>
2022-04-28 15:11:57 +0800

    自测通过

--- router4j-example/gateway/src/main/java/com/knife/gateway/dynamic/Router4jFilter.java
+++ router4j-example/gateway/src/main/java/com/knife/gateway/dynamic/Router4jFilter.java
@@ -10,6 +10,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
@@ -27,30 +28,30 @@ import java.util.Objects;
 public class Router4jFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        ServerHttpRequest req = exchange.getRequest();
+        ServerHttpRequest originalRequest = exchange.getRequest();
 
         // 可获得所有请求参数
         // Map<String, String> cachedRequestBody = exchange
         // .getAttribute(ServerWebExchangeUtils.CACHED_REQUEST_BODY_ATTR);
 
         //获取域名+端口后的path
-        String rawPath = req.getURI().getRawPath();
+        String rawPath = originalRequest.getURI().getRawPath();
 
         // todo 从redis中取出所有url，然后用rawPath去匹配
-        String hostAndPort = "http://localhost:9011";
+
+        String host = "localhost";
+        int port = 9011;
 
         URI originUri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
 
-        // 修改uri
-        URI newUri = UriComponentsBuilder.fromHttpUrl(hostAndPort + rawPath)
+        URI newUri = UriComponentsBuilder.fromUri(originUri)
+                .host(host)
+                .port(port)
                 .build()
                 .toUri();
 
         //重新封装request对象
-        ServerHttpRequest newRequest = req.mutate().uri(newUri).build();
-
-        Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
-
+        ServerHttpRequest newRequest = originalRequest.mutate().uri(newUri).build();
 
         // NettyRoutingFilter 最终从GATEWAY_REQUEST_URL_ATTR 取出uri对象进行http请求
         // 所以这里要将新的对象覆盖进去
@@ -59,55 +60,11 @@ public class Router4jFilter implements GlobalFilter, Ordered {
         return chain.filter(exchange.mutate().request(newRequest).build());
 
         // 也可以加回调方法
-        // return chain.filter(exchange.mutate().request(request).build())
+        // return chain.filter(exchange.mutate().request(newRequest).build())
         //         .then(Mono.fromRunnable(() -> {
         //             //请求完成回调方法 可以在此完成计算请求耗时等操作
         //         }));
     }
-
-    // @Override
-    // public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-    //     ServerHttpRequest req = exchange.getRequest();
-    //
-    //     // 可获得所有请求参数
-    //     // Map<String, String> cachedRequestBody = exchange
-    //     // .getAttribute(ServerWebExchangeUtils.CACHED_REQUEST_BODY_ATTR);
-    //
-    //     //获取域名+端口后的path
-    //     String rawPath = req.getURI().getRawPath();
-    //
-    //     // todo 从redis中取出所有url，然后用rawPath去匹配
-    //     String hostAndPort = "http://localhost:9011";
-    //     // 修改uri
-    //     URI uri = UriComponentsBuilder.fromHttpUrl(hostAndPort + rawPath).build().toUri();
-    //     //重新封装request对象
-    //     ServerHttpRequest newRequest = req.mutate().uri(uri).build();
-    //
-    //     Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
-    //     boolean encoded = ServerWebExchangeUtils.containsEncodedParts(uri);
-    //
-    //     URI routeUri = route.getUri();
-    //     URI mergedUri = UriComponentsBuilder.fromUri(uri)
-    //             .scheme(routeUri.getScheme())
-    //             .host(routeUri.getHost())
-    //             .port(9011)
-    //             .build(encoded)
-    //             .toUri();
-    //
-    //     URI originUri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
-    //
-    //     // NettyRoutingFilter 最终从GATEWAY_REQUEST_URL_ATTR 取出uri对象进行http请求
-    //     // 所以这里要将新的对象覆盖进去
-    //     exchange.getAttributes().put(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR, mergedUri);
-    //
-    //     return chain.filter(exchange.mutate().request(newRequest).build());
-    //
-    //     // 也可以加回调方法
-    //     // return chain.filter(exchange.mutate().request(request).build())
-    //     //         .then(Mono.fromRunnable(() -> {
-    //     //             //请求完成回调方法 可以在此完成计算请求耗时等操作
-    //     //         }));
-    // }
 
     @Override
     public int getOrder() {
