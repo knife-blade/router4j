12cf08f0449814692706a618d36f2d3f0b5ff031
water <2716552863@qq.com>
2022-05-03 20:21:43 +0800

    修改类名

---
+++ router4j-client-feign/src/main/java/com/knife/router4j/feign/dynamic/config/Router4jFeignLoadBalancerAutoConfiguration.java
@@ -0,0 +1,43 @@
+package com.knife.router4j.feign.dynamic.config;
+
+import com.knife.router4j.feign.dynamic.config.concret.Router4jDefaultClientConfiguration;
+import com.knife.router4j.feign.dynamic.config.concret.Router4jOkHttpClientConfiguration;
+import feign.Client;
+import feign.Feign;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.loadbalancer.config.BlockingLoadBalancerClientAutoConfiguration;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.cloud.openfeign.loadbalancer.FeignLoadBalancerAutoConfiguration;
+import org.springframework.cloud.openfeign.support.FeignHttpClientProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * 重要的类：
+ * Feign自动配置：{@link FeignAutoConfiguration}
+ * Feign的Client接口：{@link Client}
+ */
+// 这行是自己加的
+@AutoConfigureBefore({FeignLoadBalancerAutoConfiguration.class})
+
+@ConditionalOnClass(Feign.class)
+@ConditionalOnBean({LoadBalancerClient.class, LoadBalancerClientFactory.class})
+@AutoConfigureAfter({BlockingLoadBalancerClientAutoConfiguration.class,
+        LoadBalancerAutoConfiguration.class})
+@EnableConfigurationProperties(FeignHttpClientProperties.class)
+@Configuration(proxyBeanMethods = false)
+
+// 上边的直接拷贝自FeignLoadBalancerAutoConfiguration.class
+
+@Import({Router4jOkHttpClientConfiguration.class,
+        Router4jDefaultClientConfiguration.class})
+public class Router4jFeignLoadBalancerAutoConfiguration {
+
+}
--- router4j-client-feign/src/main/java/com/knife/router4j/feign/dynamic/config/concret/Router4jDefaultClientConfiguration.java
+++ router4j-client-feign/src/main/java/com/knife/router4j/feign/dynamic/config/concret/Router4jDefaultClientConfiguration.java
@@ -1,4 +1,4 @@
-package com.knife.router4j.feign.dynamic;
+package com.knife.router4j.feign.dynamic.config.concret;
 
 import com.knife.router4j.feign.dynamic.client.Router4jDefaultClient;
 import com.knife.router4j.feign.dynamic.client.Router4jOkHttpClient;
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerProperties;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
@@ -24,8 +25,11 @@ import org.springframework.context.annotation.Configuration;
  *      Feign自动配置：{@link FeignAutoConfiguration}
  *      Feign的Client接口：{@link Client}
  */
-@Configuration
-public class Router4jFeignConfig {
+@Configuration(proxyBeanMethods = false)
+// 没有OkHttpClient和ApacheHttpClient时才生效
+@ConditionalOnMissingClass({"feign.okhttp.OkHttpClient", "feign.httpclient.ApacheHttpClient"})
+@EnableConfigurationProperties(LoadBalancerProperties.class)
+public class Router4jDefaultClientConfiguration {
     /**
      * 自己实现了一个Client：{@link Router4jDefaultClient}
      *
@@ -37,40 +41,13 @@ public class Router4jFeignConfig {
      *      那么：我们就可以手动构造一个替换它！
      */
     @Bean
-    @ConditionalOnMissingClass({"feign.okhttp.OkHttpClient", "feign.httpclient.ApacheHttpClient"})
+    @ConditionalOnMissingBean
     @Conditional(OnRetryNotEnabledCondition.class)
     public Client feignClient(LoadBalancerClient loadBalancerClient,
                               LoadBalancerProperties properties,
                               LoadBalancerClientFactory loadBalancerClientFactory) {
         return new FeignBlockingLoadBalancerClient(
                 new Router4jDefaultClient(null, null),
-                loadBalancerClient, properties, loadBalancerClientFactory);
-    }
-
-    /**
-     * 重要的类：
-     *      Feign自动配置：{@link FeignAutoConfiguration}
-     *      OkHttpFeignLoadBalancerConfiguration#feignClient(xxx)}
-     *
-     * 为什么提供一个这个Bean就可以了呢？
-     *      原因：在Feign的接口上打断点，追到如下位置：{@link FeignBlockingLoadBalancerClient#execute(Request, Request.Options)}
-     *           它最后一行代码用到了Client delegate，它来自于构造函数：{@link FeignBlockingLoadBalancerClient#FeignBlockingLoadBalancerClient(Client, LoadBalancerClient, LoadBalancerProperties, LoadBalancerClientFactory)}
-     *           此构造函数被调用的地方：OkHttpFeignLoadBalancerConfiguration#feignClient(xxx)
-     *           它上边有个：@ConditionalOnMissingBean
-     *           那么：我们就可以手动构造一个替换它！
-     */
-    @Bean
-    @ConditionalOnMissingBean
-    @ConditionalOnClass(OkHttpClient.class)
-    @ConditionalOnProperty("feign.okhttp.enabled")
-    @Conditional(OnRetryNotEnabledCondition.class)
-    public Client feignClient(okhttp3.OkHttpClient okHttpClient,
-                              LoadBalancerClient loadBalancerClient,
-                              LoadBalancerProperties properties,
-                              LoadBalancerClientFactory loadBalancerClientFactory) {
-        Client delegate = new Router4jOkHttpClient(okHttpClient);
-
-        return new FeignBlockingLoadBalancerClient(delegate,
                 loadBalancerClient, properties, loadBalancerClientFactory);
     }
 }
