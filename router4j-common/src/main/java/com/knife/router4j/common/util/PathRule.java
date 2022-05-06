6a669abfc624fdb2c4838057bf192e4a96206f63
water <2716552863@qq.com>
2022-05-06 20:00:31 +0800

    common：修改参数名

--- router4j-common/src/main/java/com/knife/router4j/common/util/PathRule.java
+++ router4j-common/src/main/java/com/knife/router4j/common/util/PathRule.java
@@ -23,44 +23,44 @@ public class PathRule {
     private final AntPathMatcher pathMatcher = new AntPathMatcher();
 
     /**
-     * 将域名+端口号和路径绑定
-     * @param hostAndPort 域名+端口号。例如：127.0.0.1:8080
+     * 将实例和路径绑定
+     * @param instanceAddress 实例地址。例如：127.0.0.1:8080
      * @param pathPattern 路径匹配符。例如：/order/add；/order/**
      */
-    public void bind(String hostAndPort, String pathPattern) {
-        RList<String> list = redissonClient.getList(prefix + hostAndPort);
+    public void bind(String instanceAddress, String pathPattern) {
+        RList<String> list = redissonClient.getList(prefix + instanceAddress);
         if (!list.contains(pathPattern)) {
             list.add(pathPattern);
         }
     }
 
     /**
-     * 将域名+端口号和路径解除绑定
-     * @param hostAndPort 域名+端口号。例如：127.0.0.1:8080
+     * 将实例地址和路径解除绑定
+     * @param instanceAddress 实例地址号。例如：127.0.0.1:8080
      * @param pathPattern 路径匹配符。例如：/order/add；/order/**
      */
-    public void unbind(String hostAndPort, String pathPattern) {
-        RList<String> list = redissonClient.getList(prefix + hostAndPort);
+    public void unbind(String instanceAddress, String pathPattern) {
+        RList<String> list = redissonClient.getList(prefix + instanceAddress);
         list.remove(pathPattern);
     }
 
     /**
-     * 通过路径找到规则中的端口号和域名
+     * 通过路径找到规则中的实例
      * @param path 路径。例如：/order/add
-     * @return 域名+端口号。例如：127.0.0.1:8080
+     * @return 实例地址。例如：127.0.0.1:8080
      */
     public String findMatchedHostAndPort(String path) {
-        // key：host+port  value：最具体的pattern
+        // key：实例地址  value：最具体的pattern
         Map<String, String> matchedMap = new HashMap<>();
 
         RKeys keys = redissonClient.getKeys();
 
-        // 模糊查找出所有host+port的key
+        // 模糊查找出所有实例的key
         Iterable<String> hostAndPortS = keys.getKeysByPattern(prefix + "*");
 
         for (String hostAndPort : hostAndPortS) {
 
-            // 取出每个host+port的所有指定好的路径规则
+            // 取出每个实例的所有指定好的路径规则
             RList<String> pathPatterns = redissonClient.getList(hostAndPort);
             for (String pathPattern : pathPatterns) {
                 if (pathMatcher.match(pathPattern, path)) {
@@ -73,16 +73,16 @@ public class PathRule {
             }
         }
 
-        String matchedHostAndPort = null;
+        String matchedInstanceAddress = null;
         String longestPath = "";
 
         for (Map.Entry<String, String> entry : matchedMap.entrySet()) {
             if (entry.getValue().length() > longestPath.length()) {
                 longestPath = entry.getValue();
-                matchedHostAndPort = entry.getKey();
+                matchedInstanceAddress = entry.getKey();
             }
         }
 
-        return matchedHostAndPort;
+        return matchedInstanceAddress;
     }
 }
