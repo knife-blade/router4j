e32a2b48e73ee94933a4761344d25e0eeabd7e4d
water <2716552863@qq.com>
2022-05-18 14:03:37 +0800

    完善默认实例工具类

---
+++ router4j-common/src/main/java/com/knife/router4j/common/constant/PathPatternConstant.java
@@ -0,0 +1,8 @@
+package com.knife.router4j.common.constant;
+
+/**
+ * 路径模板常量
+ */
+public interface PathPatternConstant {
+    String DEFAULT_PATTERN = "/**";
+}
--- router4j-common/src/main/java/com/knife/router4j/common/util/DefaultInstanceUtil.java
+++ router4j-common/src/main/java/com/knife/router4j/common/util/DefaultInstanceUtil.java
@@ -1,37 +1,66 @@
 package com.knife.router4j.common.util;
 
+import com.knife.router4j.common.constant.PathPatternConstant;
+import com.knife.router4j.common.entity.PathRuleRequest;
 import com.knife.router4j.common.property.RuleProperties;
 import com.knife.router4j.common.redis.RedissonHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DefaultInstanceUtil {
     @Autowired
     private RuleProperties rule;
 
+    @Autowired
+    private PathRuleUtil pathRuleUtil;
+
     /**
      * 标记为默认实例
-     * @param instanceAddresses 实例地址
+     *
+     * @param pathRuleRequests 路径规则请求列表
      */
-    public void markAsDefaultInstance(List<String> instanceAddresses) {
+    public void markAsDefaultInstance(List<PathRuleRequest> pathRuleRequests) {
+        List<String> instanceAddresses = pathRuleRequests.stream()
+                .map(PathRuleRequest::getInstanceAddress)
+                .distinct()
+                .collect(Collectors.toList());
+
         RedissonHolder.getRedissonClient()
                 .getList(rule.getDefaultInstancePrefix())
                 .addAll(instanceAddresses);
+
+        for (PathRuleRequest pathRuleRequest : pathRuleRequests) {
+            pathRuleRequest.setPathPattern(PathPatternConstant.DEFAULT_PATTERN);
+            pathRuleUtil.bind(pathRuleRequest);
+        }
     }
 
     /**
      * 取消默认实例
-     * @param instanceAddresses 实例地址
+     *
+     * @param pathRuleRequests 路径规则请求列表
      */
-    public void cancelDefaultInstance(List<String> instanceAddresses) {
+    public void cancelDefaultInstance(List<PathRuleRequest> pathRuleRequests) {
+        List<String> instanceAddresses = pathRuleRequests.stream()
+                .map(PathRuleRequest::getInstanceAddress)
+                .distinct()
+                .collect(Collectors.toList());
+
         RedissonHolder.getRedissonClient()
                 .getList(rule.getDefaultInstancePrefix())
                 .removeAll(instanceAddresses);
+
+        for (PathRuleRequest pathRuleRequest : pathRuleRequests) {
+            pathRuleRequest.setPathPattern(PathPatternConstant.DEFAULT_PATTERN);
+            pathRuleUtil.unbind(pathRuleRequest);
+        }
     }
 
     /**
      * 查找所有的默认实例
+     *
      * @return 实例地址列表
      */
     public List<String> findAllDefaultInstance() {
