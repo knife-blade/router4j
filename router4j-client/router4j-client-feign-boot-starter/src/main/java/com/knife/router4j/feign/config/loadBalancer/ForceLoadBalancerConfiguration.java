a775d8236dcb19c77d9c0331a993df65af49bb2b
water <2716552863@qq.com>
2022-09-15 23:08:34 +0800

    解决找不到类的问题

--- router4j-client/router4j-client-feign-boot-starter/src/main/java/com/knife/router4j/feign/config/loadBalancer/ForceLoadBalancerConfiguration.java
+++ router4j-client/router4j-client-feign-boot-starter/src/main/java/com/knife/router4j/feign/config/loadBalancer/ForceLoadBalancerConfiguration.java
@@ -1,13 +1,9 @@
 package com.knife.router4j.feign.config.loadBalancer;
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerProperties;
-import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerBeanPostProcessorAutoConfiguration;
-import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerClientAutoConfiguration;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
+import org.springframework.cloud.loadbalancer.config.LoadBalancerAutoConfiguration;
 import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
 import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
@@ -16,7 +12,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 @Configuration(proxyBeanMethods = false)
-@AutoConfigureAfter({ LoadBalancerClientFactory.class })
+@LoadBalancerClients
+@AutoConfigureBefore({ LoadBalancerAutoConfiguration.class })
 public class ForceLoadBalancerConfiguration {
     @Bean
     public ReactorLoadBalancer<ServiceInstance> reactorServiceInstanceLoadBalancer(Environment environment,
