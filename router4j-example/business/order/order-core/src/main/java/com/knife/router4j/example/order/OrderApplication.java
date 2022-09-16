d46e52fc836fa5168b73292b701c59ed48aa14e1
water <2716552863@qq.com>
2022-09-16 18:12:11 +0800

    修改groupId等，暂时关闭对client的引用

--- router4j-example/business/order/order-core/src/main/java/com/knife/router4j/example/order/OrderApplication.java
+++ router4j-example/business/order/order-core/src/main/java/com/knife/router4j/example/order/OrderApplication.java
@@ -1,9 +1,12 @@
 package com.knife.router4j.example.order;
 
 import com.knife.router4j.example.common.annotation.CommonApplication;
+import com.knife.router4j.example.order.config.LoadBalancerClientConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClients;
 
 @CommonApplication
+@LoadBalancerClients(defaultConfiguration = LoadBalancerClientConfiguration.class)
 public class OrderApplication {
 
     public static void
