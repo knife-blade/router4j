a330ddbb53c7dd58efb67dc5c249285912591b8e
knifeout <2716552863@qq.com>
2024-02-05 17:34:32 +0800

    添加命名空间的module（vuex）

---
+++ router4j-vue/src/store/modules/namespace.js
@@ -0,0 +1,18 @@
+/**
+ * 后端注册中心的命名空间
+ */
+const state = {
+    namespaceName: ''
+}
+
+const mutations = {
+    WRITE_NAMESPACE_NAME(state, namespaceName) {
+        this.state.namespaceName = namespaceName;
+    }
+}
+
+export default {
+    namespaced: true,
+    state,
+    mutations
+}
